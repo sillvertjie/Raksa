205e579d7c81538a679f6d110a994aae15ad9b67
inone <email-sillvertjie@gmail.com>
2026-07-07 22:26:30 +0700

    PR-006 implement note repository with prisma

--- features/notes/repositories/note.repository.ts
+++ features/notes/repositories/note.repository.ts
@@ -1,1 +1,44 @@
-export class NoteRepository {}
+import { prisma } from "@/lib/prisma";
+import { CreateNoteDTO } from "../dto/create-note.dto";
+import { UpdateNoteDTO } from "../dto/update-note.dto";
+
+export class NoteRepository {
+  async create(data: CreateNoteDTO) {
+    return prisma.note.create({
+      data,
+    });
+  }
+
+  async findAll() {
+    return prisma.note.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
+
+  async findById(id: string) {
+    return prisma.note.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: string, data: UpdateNoteDTO) {
+    return prisma.note.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    return prisma.note.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
