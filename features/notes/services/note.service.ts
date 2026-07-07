6d4f5b628d0fbb6f315655bc1ee0e222ab8a625f
inone <email-sillvertjie@gmail.com>
2026-07-07 22:36:36 +0700

    PR-007 implement note service layer

--- features/notes/services/note.service.ts
+++ features/notes/services/note.service.ts
@@ -1,1 +1,31 @@
-export class NoteService {}
+import { NoteRepository } from "../repositories/note.repository";
+import { CreateNoteDTO } from "../dto/create-note.dto";
+import { UpdateNoteDTO } from "../dto/update-note.dto";
+
+export class NoteService {
+  private readonly noteRepository: NoteRepository;
+
+  constructor() {
+    this.noteRepository = new NoteRepository();
+  }
+
+  async create(data: CreateNoteDTO) {
+    return this.noteRepository.create(data);
+  }
+
+  async findAll() {
+    return this.noteRepository.findAll();
+  }
+
+  async findById(id: string) {
+    return this.noteRepository.findById(id);
+  }
+
+  async update(id: string, data: UpdateNoteDTO) {
+    return this.noteRepository.update(id, data);
+  }
+
+  async delete(id: string) {
+    return this.noteRepository.delete(id);
+  }
+}
