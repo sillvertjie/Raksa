ff5d392f02443cfd8b4c5f360dd7af2f2fe64c5f
inone <email-sillvertjie@gmail.com>
2026-07-09 13:18:28 +0700

    refactor(card): integrate design system tokens

--- components/ui/layout/Card.tsx
+++ components/ui/layout/Card.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
+import {
+  radius,
+  spacing,
+} from "@/lib/design-system";
+
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
-export default function Card(props: Props) {
+export default function Card({
+  className,
+  ...props
+}: Props) {
   return (
     <div
       {...props}
-      className={`rounded-lg border p-4 ${props.className ?? ""}`}
+      className={`
+        border
+        ${radius.lg}
+        ${spacing.cardPaddingSm}
+        ${className ?? ""}
+      `}
     />
   );
 }
