8d50808d8c1a4aa131d9577012041d8b4bd0edd9
inone <email-sillvertjie@gmail.com>
2026-07-09 19:49:56 +0700

    DEV-044 Authentication Foundation

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+
+  session: {
+    strategy: "database",
+  },
+
+  pages: {
+    signIn: "/login",
+  },
+
+  providers: [],
+});
